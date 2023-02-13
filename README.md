ede55ae4bc493980f3a4bd508ab3cbe5a45c0f44
Andrei Diaconu <diaconuionutandrei@gmail.com>
2023-02-13 14:17:33 +0200

    [main] Initial commit.

---
+++ README.md
@@ -0,0 +1,29 @@
+# web-starter-template
+
+Starter template for HTML, CSS & vanilla JS websites. Using Gulp.js as the build system.
+
+## 🏃 Running the website
+
+Using VSCode, you should install the [Live Server extension](https://marketplace.visualstudio.com/items?itemName=ritwickdey.LiveServer).
+
+Run
+
+`npm i`
+
+then open either HTML file with **Live Server**.
+
+## 🏗️ Building the website
+
+The build system is using Gulp.js to minify & copy the required assets.
+
+⚠️Before pushing to master, run
+
+`npm run build`
+
+and test the website from `/dist` folder using **Live Server**.
+
+## 🧬 Tech Stack
+
+HTML, CSS, JavaScript
+
+Gulp.js - Build System
---
+++ gulpfile.mjs
@@ -0,0 +1,80 @@
+'use strict';
+
+import gulp from 'gulp';
+const { src, dest, task, series } = gulp;
+import useref from 'gulp-useref';
+import terser from 'gulp-terser';
+import gulpIf from 'gulp-if';
+import { deleteAsync } from 'del';
+import cssnano from 'gulp-cssnano';
+// import cwebp from 'gulp-cwebp';
+import imagemin from 'gulp-imagemin';
+import htmlmin from 'gulp-htmlmin';
+import autoprefixer from 'gulp-autoprefixer';
+// import replace from 'gulp-replace';
+import purgecss from 'gulp-purgecss';
+/* eslint-enable no-undef */
+
+const DIR = {
+  src: 'src',
+  dist: 'dist',
+};
+
+function cleanup() {
+  return deleteAsync([DIR.dist]);
+}
+
+function minify() {
+  return (
+    src(`${DIR.src}/*.html`)
+      .pipe(useref())
+      .pipe(gulpIf('*.html', htmlmin({ collapseWhitespace: true })))
+      // .pipe(replace('png', 'webp'))
+      // .pipe(replace('jpg', 'webp'))
+      .pipe(gulpIf('*.js', terser()))
+      .pipe(
+        gulpIf(
+          '*.css',
+          purgecss({
+            content: ['src/**/*.html'],
+          })
+        )
+      )
+      .pipe(gulpIf('*.css', cssnano()))
+      .pipe(gulpIf('*.css', autoprefixer()))
+      .pipe(dest(DIR.dist))
+  );
+}
+
+function components() {
+  return src(`${DIR.src}/components/**.js`)
+    .pipe(terser())
+    .pipe(dest(`${DIR.dist}/components`));
+}
+
+function copyImages() {
+  return (
+    src(`${DIR.src}/assets/images/**/*`)
+      .pipe(gulpIf('*.+(png|jpg|gif|svg)', imagemin()))
+      // .pipe(gulpIf('*.+(png|jpg)', webp()))
+      .pipe(dest(`${DIR.dist}/assets/images`))
+  );
+}
+
+function copyDocs() {
+  return src(`${DIR.src}/assets/docs/**/*`).pipe(
+    dest(`${DIR.dist}/assets/docs`)
+  );
+}
+
+function copyMiscFiles() {
+  return src([
+    `${DIR.src}/robots.txt`,
+    `${DIR.src}/sitemap.xml`
+  ]).pipe(dest(DIR.dist));
+}
+
+task(
+  'build',
+  series(cleanup, copyImages, copyDocs, minify, components, copyMiscFiles)
+);
